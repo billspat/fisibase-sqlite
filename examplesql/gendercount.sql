aadabc0fb0adf5656d2efe597c29f8966b90784d
Pat Bills <pat@billspat.com>
2016-01-08 21:23:07 -0500

    addt'l example sql

--- examplesql/gendercount.sql
+++ examplesql/gendercount.sql
@@ -1,5 +1,5 @@
 -- fisibase sqlite
 -- example sql 'query'
--- for each 'sex' in hyenas table, count number of m, f born in any natal clan
+-- for each 'sex' in hyenas table, count number of m, f born in any natal clan, group by clan
 -- includes animals with no gender entered (cubs died before id)
- select sex,count(*) as n from tblHyenas where mom is not null group by sex;
+ select clan,sex,count(*) as n from tblHyenas where mom is not null group by clan, sex;
